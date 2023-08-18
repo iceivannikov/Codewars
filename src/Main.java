3060da5aa523d8e35061eb395ed6e51d291c1c14
Viktor Ivannikov <iceivannikov@gmail.com>
2023-08-18 15:27:10 +0300

    Task: Mumbling

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,32 @@
-import java.util.Arrays;
-
 /**
- * You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
- * Find max(abs(length(x) − length(y)))
- * If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
- * Example:
- * a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
- * a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
- * mxdiflg(a1, a2) --> 13
- * Bash note:
- * input : 2 strings with substrings separated by ,
- * output: number as a string
+ * This time no story, no theory. The examples below show you how to write function accum:
+ * Examples:
+ * accum("abcd") -> "A-Bb-Ccc-Dddd"
+ * accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+ * accum("cwAt") -> "C-Ww-Aaa-Tttt"
+ * The parameter of accum is a string which includes only letters from a..z and A..Z.
  */
+
 public class Main {
 
-    public static int mxdiflg(String[] a1, String[] a2) {
-        if (a1.length == 0 || a2.length == 0) {
-            return -1;
+    public static String accum(String s) {
+        StringBuilder result = new StringBuilder();
+        int counter = 0;
+        String[] split = s.toUpperCase().split("");
+        for (String value : split) {
+            if (counter > 0) {
+                result.append("-");
+            }
+            result.append(value);
+            result.append(value.toLowerCase().repeat(counter));
+            counter++;
         }
-
-        int a1max = Arrays.stream(a1).mapToInt(String::length).max().orElse(-1);
-        int a1min = Arrays.stream(a1).mapToInt(String::length).min().orElse(-1);
-        int a2max = Arrays.stream(a2).mapToInt(String::length).max().orElse(-1);
-        int a2min = Arrays.stream(a2).mapToInt(String::length).min().orElse(-1);
-
-
-        return Math.max(Math.abs(a2max - a1min), Math.abs(a1max - a2min));
+        return result.toString();
     }
 
 
     public static void main(String[] args) {
-        String[] s1 = new String[]{"hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"};
-        String[] s2 = new String[]{"cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"};
-
-        System.out.println(mxdiflg(s1, s2));
+        String str = "ZpglnRxqenU";
+        System.out.println(accum(str));
     }
 }
