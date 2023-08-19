683076d85abfed69618c29b3fc91b68852f76856
Viktor Ivannikov <iceivannikov@gmail.com>
2023-08-19 14:15:16 +0300

    Task: Simple validation of a username with regex

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * This time no story, no theory. The examples below show you how to write function accum:
- * Examples:
- * accum("abcd") -> "A-Bb-Ccc-Dddd"
- * accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
- * accum("cwAt") -> "C-Ww-Aaa-Tttt"
- * The parameter of accum is a string which includes only letters from a..z and A..Z.
+ * Write a simple regex to validate a username. Allowed characters a
+ * lowercase lette
+ * numbe
+ * undersc
+ * Length should be between 4 and 16 characters (both included).
  */
 
 public class Main {
 
-    public static String accum(String s) {
-        StringBuilder result = new StringBuilder();
-        int counter = 0;
-        String[] split = s.toUpperCase().split("");
-        for (String value : split) {
-            if (counter > 0) {
-                result.append("-");
-            }
-            result.append(value);
-            result.append(value.toLowerCase().repeat(counter));
-            counter++;
-        }
-        return result.toString();
+    public static boolean validateUsr(String s) {
+        String regex = "[a-z0-9_]{4,16}+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        return matcher.matches();
     }
 
 
     public static void main(String[] args) {
-        String str = "ZpglnRxqenU";
-        System.out.println(accum(str));
+        String str = "a";
+        System.out.println(validateUsr(str));
     }
 }
